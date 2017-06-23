60fd1539db784f58d69814bb49efebb834432850
Alvin James Bellero <ajames.bellero@gmail.com>
2017-06-23 14:54:33 +0800

    Added authentication example

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <div> {{ (user | async)?.displayName }} </div>
+    <button (click)="signIn()">Sign In with Google</button>
+    <button (click)="signOut()">Sign Out</button>
+  `,
+  styleUrls: ['./app.component.css'],
+  providers: [AngularFireAuth]
 })
 export class AppComponent {
-  title = 'app works!';
+  user: Observable<firebase.User>;
+
+  constructor(public afAuth: AngularFireAuth) {
+    this.user = afAuth.authState;
+  }
+
+  signIn() {
+    this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
+    console.log(this.user);
+  }
+
+  signOut() {
+    this.afAuth.auth.signOut();
+    console.log(this.user)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,8 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { Config } from './app.config';
+import { AngularFireModule } from 'angularfire2';
 
 @NgModule({
   declarations: [
@@ -12,7 +14,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(Config.firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
