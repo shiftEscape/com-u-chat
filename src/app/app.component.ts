c31ebc9436b15c805b5fa4522ce029296139d069
Alvin James Bellero <ajames.bellero@gmail.com>
2017-06-23 22:24:13 +0800

    Updated Chat application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,32 +1,64 @@
 import { Component } from '@angular/core';
 import { Observable } from 'rxjs/Observable';
-import { AngularFireAuth } from 'angularfire2/auth';
 import * as firebase from 'firebase/app';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuth } from 'angularfire2/auth';
 
 @Component({
   selector: 'app-root',
-  template: `
-    <div> {{ (user | async)?.displayName }} </div>
-    <button (click)="signIn()">Sign In with Google</button>
-    <button (click)="signOut()">Sign Out</button>
-  `,
+  templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers: [AngularFireAuth]
+  providers: [AngularFireAuth, AngularFireDatabase]
 })
 export class AppComponent {
-  user: Observable<firebase.User>;
 
-  constructor(public afAuth: AngularFireAuth) {
-    this.user = afAuth.authState;
+  user: firebase.User;
+  items: FirebaseListObservable<any>;
+  name: any;
+  msgVal: string = '';
+  isLoading: boolean = true;
+
+  constructor(public af: AngularFireDatabase, public afAuth: AngularFireAuth) {
+
+    this.items = af.list('/messages', {
+      query: {
+        limitToLast: 5
+      }
+    });
+
+    afAuth.authState.subscribe((user: firebase.User) => {
+      this.user = (!user ? null : user);
+      this.isLoading = false;
+    });
+
   }
 
   signIn() {
     this.afAuth.auth.signInWithPopup(new firebase.auth.GoogleAuthProvider());
-    console.log(this.user);
   }
 
   signOut() {
     this.afAuth.auth.signOut();
-    console.log(this.user)
   }
+
+  sendChat(inputMessage: string) {
+
+    // Fail fast: Restrict saving empty chat message
+    if(inputMessage.trim() == '') {
+      this.msgVal = '';
+      return;
+    }
+
+    // Push | Save message to Firebase DB
+    this.items.push({
+      message: inputMessage,
+      name: this.user.displayName,
+      likes: 0,
+      createdAt: (new Date).toString()
+    });
+
+    // Clear chat field
+    this.msgVal = '';
+
+  } 
 }
